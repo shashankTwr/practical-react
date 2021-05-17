38893d65f09d9fdaf49c85deb5a16aaf8d727b10
shashankTwr <shashanktwr007@gmail.com>
2021-05-17 19:49:01 +0530

    component lifecycle

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import React, {Component} from "react";
 import './App.css';
-import ImageSlider from "./components/imageslider";
 import Counter from "./components/counter";
 
 
@@ -9,17 +8,13 @@ export default class App extends Component{
     visible: true
   }
   render () {
-      if(!this.state.visible){
-        return <div> display nothing</div>;
-      }
-      const buttonText = this.state.visible ? "hide" :"show";
-      const slider = this.state.visible ? <ImageSlider/> : (
-        <div><Counter initialCount = {0}/></div>
-      );
-      // console.log("counter started");
+    const buttonText = this.state.visible ? "toggle counter off"
+    : "toggle counter on";
+    const toggleCounter = this.state.visible ? <Counter initialCount={10}/>
+  : <div> counter turned off</div>
       return (
       <div className="App">
-        {slider}
+        {toggleCounter}
         <button onClick= {
             () =>{
               this.setState({visible:!this.state.visible});
@@ -31,25 +26,7 @@ export default class App extends Component{
   }
 }
 
-
-// ways to conditionally render by using a visible props
-// you can inline it or the other way is to do it using a variable
-// way 1
-// render () {
-//     console.log("counter started");
-//     return (
-//     <div className="App">
-//       {this.state.visible ? <ImageSlider/> : null}
-//       <button onClick= {
-//           () =>{
-//             this.setState({visible:!this.state.visible});
-//           }
-//         }>click to hide</button>
-//     </div>
-//
-//   );
-// }
-// use if with declaring the variable before with it like a let statement
-//  and then conditioning it with an if stattemen t
-//  to display multiple statements using if and else if with
-// this.state.whichcomponent to show and use that to work with it
+// toggle counter introduced to work with the idea of component lifecycle
+// using mounting and Unmounting way to select it
+// conditionally styling it to show the component
+// style of hidden class as none
